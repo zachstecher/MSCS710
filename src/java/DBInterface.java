adf03ee0a4eb9a376b839155a3393860084548ce
zachstecher <malleablemedia@gmail.com>
2017-03-23 14:00:10 -0400

    SQLite CREATE TABLE working, need to insert values

--- src/java/DBInterface.java
+++ src/java/DBInterface.java
@@ -1,6 +1,28 @@
+import java.sql.*;
 
 public class DBInterface {
   DBInterface(){
     System.out.println("DBInterface");
+    
+  }
+  
+  public static void createNewTable(){
+    String url = "jdbc:sqlite:tests.db";
+    
+    String sql = "CREATE TABLE IF NOT EXISTS volatile (\n"
+        + "mem_module_num integer PRIMARY KEY NOT NULL,\n"
+        + "total_memory float,\n"
+        + "speed float\n"
+        + ")";
+    
+    try (Connection conn = DriverManager.getConnection(url);
+        Statement stmt = conn.createStatement()){
+      
+      stmt.execute(sql);
+    }
+    catch (SQLException e) {
+      System.out.println(e.getMessage());
+    }
   }
 }
+
