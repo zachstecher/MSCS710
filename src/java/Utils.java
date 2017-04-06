357932800eec149dad809f04fd2039c91e2779fc
Matt <matthewsokoloff@gmail.com>
2017-04-06 12:28:29 -0400

    Created method for reading system files

--- src/java/Utils.java
+++ src/java/Utils.java
@@ -1,5 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class Utils {
-  Utils(){
-    System.out.println("Utils");
-  }
+public static BufferedReader br = null;
+public static FileReader fr = null;
+
+public static ArrayList<String> readFile(String path){
+    ArrayList<String> lines = new ArrayList<>(); 
+    try {
+	fr = new FileReader(FILENAME);
+	br = new BufferedReader(fr);
+	String sCurrentLine;
+	br = new BufferedReader(new FileReader(FILENAME));
+	while ((sCurrentLine = br.readLine()) != null) {
+	    lines.add(sCurrentLine);
+        }
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+	try {
+          if (br != null)
+	    br.close();
+	  if (fr != null)
+	    fr.close();
+	} catch (IOException ex) {
+	ex.printStackTrace();
+    }
+} 
+return lines;
+} 
 }
