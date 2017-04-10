f066fa0837bc8dbdcbb562b5d249adde1eb25f69
Matt <matthewsokoloff@gmail.com>
2017-04-10 19:05:06 -0400

    Fixed metricAggregator method for cpu information

--- src/java/MetricsAggregator1.java
+++ src/java/MetricsAggregator1.java
@@ -1,24 +1,34 @@
 //Copy of Metrics Aggregator so we can work concurrently
-public class MetricsAggregator {
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class MetricsAggregator1 {
   
-  MetricsAggregator(){
+  MetricsAggregator1(){
     System.out.println("MetricsAggregator");
   }
   
   public HashMap getStaticCPUMetrics(){
     HashMap metrics = new HashMap();
-    ArrayList<String> data = readFile("/proc/cpuinfo");
+    ArrayList<String> data = Utils.readFile("/proc/cpuinfo");
     ArrayList<String> cpuNumber = new ArrayList<>();
     ArrayList<String> maxClockRate = new ArrayList<>();
     ArrayList<String> maxTemp = new ArrayList<>();
-    ArrayList<String> tempInfo = execShell("sensors");
-    ArrayList<String>[] lists = {cpuNumber,maxClockRate};
+    ArrayList<String> tempInfo = Utils.execShell("sensors");
+    ArrayList<String>[] lists = new ArrayList[2]; 
+    lists[0] = cpuNumber;
+    lists[1] = maxClockRate;
+
+
+
     for(String line : data){
     String result = "";
     String[] keys = {"processor", "model name"};
     String[] patterns = {"[0-9]+","[0-9]+.[0-9]"};
        for (int i = 0; i < keys.length; i++){
-       String result_sub  = getValue(line, keys[i], patterns[i])
+       String result_sub  = Utils.getValue(line, keys[i], patterns[i]);
        if(result_sub != null){
           lists[i].add(result_sub);
        }
@@ -33,13 +43,14 @@ public class MetricsAggregator {
        value = matcher.group();
      }
      System.out.println(value);
-     max_temp.add(value);
-     max_temp.add(value);
+     maxTemp.add(value);
+     maxTemp.add(value);
     }
 
 
    metrics.put("cpu_number", cpuNumber);
    metrics.put("max_clock_rate", maxClockRate);
-   metrics.put("max_temp",max_temp);
+   metrics.put("max_temp",maxTemp);
+   return metrics;
   }
 }
