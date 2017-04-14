8b4a505faf3869e9d03e4eb12fc3ed413db14c26
Matt <matthewsokoloff@gmail.com>
2017-04-14 18:12:46 -0400

    Finished php script

--- src/php/getData.php
+++ src/php/getData.php
@@ -1,21 +1,22 @@
 <?php
+  if(isset($_GET["table"])){
    class MyDB extends SQLite3
    {
       function __construct()
       {
-         $this->open('test.db');
+         $this->open('tests.db');
       }
    }
    $db = new MyDB();
    if(!$db){
       echo $db->lastErrorMsg();
-   } else {
-      echo "Opened database successfully\n";
    }
-
-   $sql =<<<EOF
-      SELECT * from cpu_interrupts;
-EOF;
+   if(isset($_GET["WHERE"])){
+    // Use underscores for the where
+    $sql = "SELECT * from". $_GET["table"]." where ". str_replace($_GET["WHERE"],"_"," ");
+   }else{
+         $sql ="SELECT * from " .$_GET["table"];
+   }
 
    $ret = $db->query($sql);
    while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
@@ -26,8 +27,8 @@ EOF;
       /*Last element plus newline*/
       echo array_values(array_slice($row, -1))[0] . "\n";
    }
-   echo "Operation done successfully\n";
    $db->close();
+}
 ?>
 
 
