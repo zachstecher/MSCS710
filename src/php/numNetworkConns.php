5a67a4b3b714a4c2c2452f63efc3bac42d4ca41f
Matt <matthewsokoloff@gmail.com>
2017-04-16 09:49:20 -0400

    Basic website is complete. Needs some bells and whistles

--- src/php/numNetworkConns.php
+++ src/php/numNetworkConns.php
@@ -10,16 +10,16 @@
    if(!$db){
       echo $db->lastErrorMsg();
    }
-    $sql = "SELECT * from (select distinct disk_name from persistent_storage) a ";
+    $sql = "SELECT count(*), timestamp from networking_stats group by timestamp";
 
    $ret = $db->query($sql);
    while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
        /*Everything except the last element */
        foreach(array_slice($row, 0, -1) as $col){
-       echo str_replace("/","_",$col) . ",";
+       echo $col . ",";
        };
       /*Last element plus newline*/
-      echo str_replace("/","-",array_values(array_slice($row, -1))[0]) . "\n";
+      echo array_values(array_slice($row, -1))[0] . "|";
    }
    $db->close();
 ?>
